f2dd35ff8937499b8d6b0040ba29b22d724ed33d
Mike Kong <miketkong@gmail.com>
2026-06-02 15:38:47 -1000

    Completed first pass of new Shark Diving tour landing  page.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addShortcode("cssVersion", function(filePath) {
+    try {
+      const stats = fs.statSync(path.join(__dirname, filePath));
+      return Math.floor(stats.mtimeMs);
+    } catch(e) {
+      return Date.now();
+    }
+  });
+
   eleventyConfig.addPassthroughCopy("articles");
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("assets/video");
